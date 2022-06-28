9d092f9916a64830c16bfc8611c95f4e141faee8
lukas <lukas.halza@gmail.com>
2022-06-28 23:34:17 +0100

    Update Color Test

--- src/main/java/fine/fractals/color/things/ColorTest.java
+++ src/main/java/fine/fractals/color/things/ColorTest.java
@@ -6,31 +6,36 @@ import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.awt.image.BufferedImage;
 
+import static java.awt.image.BufferedImage.TYPE_INT_RGB;
+import static javax.swing.WindowConstants.EXIT_ON_CLOSE;
+
 public class ColorTest extends JComponent {
 
-    private static JFrame frame;
-    private static Palette palette;
-    private static Integer resolution = null;
-    private boolean painted = false;
-    private BufferedImage testImage = null;
+    private final JFrame frame;
+    private final Palette palette;
+    private final Integer resolution;
+    private final BufferedImage testImage;
+    private final int height = 100;
 
-    private ColorTest() {
-    }
+    private boolean painted = false;
 
     public static void execute(Palette palette) {
-        resolution = palette.colorResolution();
+        new ColorTest(palette);
+    }
 
-        ColorTest.palette = palette;
-        ColorTest app = new ColorTest();
+    private ColorTest(Palette palette) {
+        this.palette = palette;
+        this.resolution = palette.colorResolution();
+        this.testImage = new BufferedImage(resolution, height, TYPE_INT_RGB);
 
-        frame = new JFrame("ColorTest: " + Palette.getName());
-        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        frame.getContentPane().add(app);
-        frame.pack();
-        frame.setLocationByPlatform(true);
-        frame.setVisible(true);
+        this.frame = new JFrame("ColorTest: " + Palette.getName());
+        this.frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.frame.getContentPane().add(this);
+        this.frame.pack();
+        this.frame.setLocationByPlatform(true);
+        this.frame.setVisible(true);
 
-        frame.addKeyListener(new KeyAdapter() {
+        this.frame.addKeyListener(new KeyAdapter() {
             @Override
             public void keyPressed(KeyEvent ke) {
                 if (ke.getKeyCode() == KeyEvent.VK_ESCAPE) {
@@ -40,33 +45,25 @@ public class ColorTest extends JComponent {
         });
     }
 
-    private BufferedImage applyTestColorPalette() {
-
-        BufferedImage testImage = new BufferedImage(resolution, 100, BufferedImage.TYPE_INT_RGB);
-
-        for (int y = 0; y < 100; y++) {
-            for (int x = 0; x < resolution; x++) {
-                testImage.setRGB(x, y, palette.getSpectrumValue(x).getRGB());
-            }
-        }
-        return testImage;
-    }
-
     @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
         Graphics2D g2d = (Graphics2D) g.create();
         if (!painted) {
             painted = true;
-            testImage = applyTestColorPalette();
+            for (int y = 0; y < height; y++) {
+                for (int x = 0; x < resolution; x++) {
+                    testImage.setRGB(x, y, palette.getSpectrumValue(x).getRGB());
+                }
+            }
         }
-        g2d.drawImage(testImage, 0, 0, resolution, 100, null);
+        g2d.drawImage(testImage, 0, 0, resolution, height, null);
         g2d.dispose();
     }
 
     @Override
     public Dimension getPreferredSize() {
-        return new Dimension(resolution, 100);
+        return new Dimension(resolution, height);
     }
 
 }
