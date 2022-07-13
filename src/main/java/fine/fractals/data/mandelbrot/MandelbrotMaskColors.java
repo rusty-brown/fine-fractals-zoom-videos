58d11ac4ffca0bae7f63fc0095906b2aed31865e
lukas <lukas.halza@gmail.com>
2022-07-13 11:14:59 +0100

    Improve Mandelbrot colors

--- src/main/java/fine/fractals/data/mandelbrot/MandelbrotMaskColors.java
+++ src/main/java/fine/fractals/data/mandelbrot/MandelbrotMaskColors.java
@@ -6,14 +6,14 @@ public class MandelbrotMaskColors {
 
     public static final Color ACTIVE_NEW = new Color(40, 180, 150);
     public static final Color FINISHED_TOO_LONG = new Color(0, 0, 0);
-    public static final Color HIBERNATED_DEEP_BLACK = new Color(40, 40, 40);
+    public static final Color HIBERNATED_DEEP_BLACK = new Color(90, 90, 90);
     public static final Color FINISHED_TOO_SHORT = new Color(220, 220, 240);
     public static final Color FINISHED_SUCCESS = new Color(255, 0, 0);
-    public static final Color FINISHED_SUCCESS_PAST = new Color(92, 0, 0);
+    public static final Color FINISHED_SUCCESS_PAST = new Color(130, 100, 130);
 
     /**
      * For refresh during Mandelbrot pixels recalculation.
      */
-    public static final Color NULL = new Color(90, 90, 99);
+    public static final Color NULL = new Color(40, 80, 100);
 
 }
