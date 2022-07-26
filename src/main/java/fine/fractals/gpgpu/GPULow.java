ddc971537f351e9f971a786e5aee1bdd29d30316
lukas <lukas.halza@gmail.com>
2022-07-26 17:08:39 +0100

    Auto-format code

--- src/main/java/fine/fractals/gpgpu/GPULow.java
+++ src/main/java/fine/fractals/gpgpu/GPULow.java
@@ -28,63 +28,63 @@ import static org.jocl.CL.setExceptionsEnabled;
 
 abstract sealed class GPULow permits GPUHigh {
 
-	private static final Logger log = LogManager.getLogger(GPULow.class);
+    private static final Logger log = LogManager.getLogger(GPULow.class);
 
-	final protected cl_context context;
-	final protected cl_command_queue commandQueue;
-	final protected cl_program PROGRAM;
-	final protected cl_kernel KERNEL;
-	final protected cl_device_id device;
+    final protected cl_context context;
+    final protected cl_command_queue commandQueue;
+    final protected cl_program PROGRAM;
+    final protected cl_kernel KERNEL;
+    final protected cl_device_id device;
 
-	protected GPULow() {
+    protected GPULow() {
 
-		final int platformIndex = 0;
-		final int deviceIndex = 0;
+        final int platformIndex = 0;
+        final int deviceIndex = 0;
 
-		setExceptionsEnabled(true);
+        setExceptionsEnabled(true);
 
-		// Obtain the number of platforms
-		int[] numPlatformsArray = new int[GPU_COUNT];
-		clGetPlatformIDs(0, null, numPlatformsArray);
-		int numPlatforms = numPlatformsArray[0];
-		log.info("numPlatforms " + numPlatforms);
+        // Obtain the number of platforms
+        int[] numPlatformsArray = new int[GPU_COUNT];
+        clGetPlatformIDs(0, null, numPlatformsArray);
+        int numPlatforms = numPlatformsArray[0];
+        log.info("numPlatforms " + numPlatforms);
 
-		// Obtain a platform ID
-		cl_platform_id[] platforms = new cl_platform_id[numPlatforms];
-		clGetPlatformIDs(platforms.length, platforms, null);
-		cl_platform_id platform = platforms[platformIndex];
-		log.info("platform " + platform);
+        // Obtain a platform ID
+        cl_platform_id[] platforms = new cl_platform_id[numPlatforms];
+        clGetPlatformIDs(platforms.length, platforms, null);
+        cl_platform_id platform = platforms[platformIndex];
+        log.info("platform " + platform);
 
-		// Initialize the context properties
-		cl_context_properties contextProperties = new cl_context_properties();
-		contextProperties.addProperty(CL_CONTEXT_PLATFORM, platform);
+        // Initialize the context properties
+        cl_context_properties contextProperties = new cl_context_properties();
+        contextProperties.addProperty(CL_CONTEXT_PLATFORM, platform);
 
-		// Obtain the number of devices for the platform
-		int[] numDevicesArray = new int[1];
-		clGetDeviceIDs(platform, CL_DEVICE_TYPE_ALL, 0, null, numDevicesArray);
-		int numDevices = numDevicesArray[0];
+        // Obtain the number of devices for the platform
+        int[] numDevicesArray = new int[1];
+        clGetDeviceIDs(platform, CL_DEVICE_TYPE_ALL, 0, null, numDevicesArray);
+        int numDevices = numDevicesArray[0];
 
-		// Obtain a device ID
-		cl_device_id[] devices = new cl_device_id[numDevices];
-		clGetDeviceIDs(platform, CL_DEVICE_TYPE_ALL, numDevices, devices, null);
-		this.device = devices[deviceIndex];
+        // Obtain a device ID
+        cl_device_id[] devices = new cl_device_id[numDevices];
+        clGetDeviceIDs(platform, CL_DEVICE_TYPE_ALL, numDevices, devices, null);
+        this.device = devices[deviceIndex];
 
-		for (int i = 0; i < numDevices; i++) {
-			log.debug("Device " + (i + 1) + " of " + numDevices + ": " + getDeviceName(devices[i]));
-		}
+        for (int i = 0; i < numDevices; i++) {
+            log.debug("Device " + (i + 1) + " of " + numDevices + ": " + getDeviceName(devices[i]));
+        }
 
-		context = clCreateContext(contextProperties, 1, new cl_device_id[]{device}, null, null, null);
-		commandQueue = clCreateCommandQueueWithProperties(context, device, null, null);
-		PROGRAM = clCreateProgramWithSource(context, 1, new String[]{PROGRAM_SOURCE_CODE}, null, null);
-		clBuildProgram(PROGRAM, 0, null, null, null, null);
-		KERNEL = clCreateKernel(PROGRAM, "calculateFractalValues", null);
-	}
+        context = clCreateContext(contextProperties, 1, new cl_device_id[]{device}, null, null, null);
+        commandQueue = clCreateCommandQueueWithProperties(context, device, null, null);
+        PROGRAM = clCreateProgramWithSource(context, 1, new String[]{PROGRAM_SOURCE_CODE}, null, null);
+        clBuildProgram(PROGRAM, 0, null, null, null, null);
+        KERNEL = clCreateKernel(PROGRAM, "calculateFractalValues", null);
+    }
 
-	private String getDeviceName(cl_device_id device) {
-		long[] size = new long[1];
-		clGetDeviceInfo(device, CL_DEVICE_NAME, 0, null, size);
-		byte[] buffer = new byte[(int) size[0]];
-		clGetDeviceInfo(device, CL_DEVICE_NAME, buffer.length, Pointer.to(buffer), null);
-		return new String(buffer, 0, buffer.length - 1);
-	}
+    private String getDeviceName(cl_device_id device) {
+        long[] size = new long[1];
+        clGetDeviceInfo(device, CL_DEVICE_NAME, 0, null, size);
+        byte[] buffer = new byte[(int) size[0]];
+        clGetDeviceInfo(device, CL_DEVICE_NAME, buffer.length, Pointer.to(buffer), null);
+        return new String(buffer, 0, buffer.length - 1);
+    }
 }
